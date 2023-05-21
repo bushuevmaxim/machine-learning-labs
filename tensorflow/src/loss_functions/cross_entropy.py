547991c2255f833634301244d29ccbda9a001a18
Max Bushuev <bushuevmaxim55@gmail.com>
2023-05-21 07:39:24 +0600

    d

--- tensorflow/src/loss_functions/cross_entropy.py
+++ tensorflow/src/loss_functions/cross_entropy.py
@@ -1,13 +1,18 @@
 from loss_functions.loss_function import *
 
+
 class BinaryCrossEntropy(LossFunction):
     def fun(y_true, y_pred):
-        return -(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred)).mean()
-    
-    def derivative(y_true,y_pred):
-        
-        return (-y_true/y_pred) + (1 - y_true)/(1 - y_pred)
-    
+        epsilon = 1e-8
+        y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+        loss = -(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
+        return np.mean(loss)
+
+    def derivative(y_true, y_pred):
+        epsilon = 1e-8
+        y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+        return (y_pred - y_true) / ((1 - y_pred) * y_pred)
+
 
 def _softmax(X):
     exps = np.exp(X)
